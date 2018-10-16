ef3c6f033488872b0f10882202ee1fba96933c9e
Rahul <escholsystems@gmail.com>
2018-10-16 12:48:34 +0530

    added

---
+++ main.js
@@ -0,0 +1,3 @@
+input type="text" name="Consignment No:" size="30  " id= "Consignment No:" class="" name="Consignment No:" type="text" 
+ value="Consignment No:" onfocus="if (this.value=='Consignment No:') {this.value='';}"
+  onblur="if (this.value=='')">              
